3bf456c68bfe5df6affeffaed123f4b52e42a543
rkeesari98 <rkeesari98@gmail.com>
2025-03-20 10:07:49 +0530

    feat: removed unused imports

--- team_service.py
+++ team_service.py
@@ -1,15 +1,13 @@
 from models import Driver
 from typing import Dict, List, Optional, Union
-from fastapi import FastAPI, Request,Form, Depends
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-import google.oauth2.id_token
+
+
+
 from google.auth.transport import requests
 from google.cloud import firestore
 from models import Driver, Team,Query
-import starlette.status as status
-from fastapi.responses import RedirectResponse
+
+
 
 
 firestore_db = firestore.Client()
